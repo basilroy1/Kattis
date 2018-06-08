56738b94db5380594388587f921428a634582a48
Basil Roy <basil.k.roy@hotmail.com>
2018-06-08 16:04:52 +0100

    Bijele.java

---
+++ Main (4).java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main 
+{
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    int num2 = scan.nextInt();
+    int num3 = scan.nextInt();
+    int num4 = scan.nextInt();
+    int num5 = scan.nextInt();
+    int num6 = scan.nextInt();
+    
+    
+    int z=0;
+    int z2=0;
+    int z3=0;
+    int z4=0;
+    int z5=0;
+    int z6=0;
+    
+    int y=1;
+    int y2=1;
+    int y3=2;
+    int y4=2;
+    int y5=2;
+    int y6=8;
+    
+    z=y-num;
+    z2=y2-num2;
+    z3=y3-num3;
+    z4=y4-num4;
+    z5=y5-num5;
+    z6=y6-num6;
+        
+    System.out.println(z);
+    System.out.println(z2);
+    System.out.println(z3);
+    System.out.println(z4);
+    System.out.println(z5);
+    System.out.println(z6);
+    
+    
+  }
+}
