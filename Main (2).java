d0a5770e565d35d07e369173df8303af23d6114b
Basil Roy <basil.k.roy@hotmail.com>
2018-06-08 16:02:22 +0100

    faktor.java

---
+++ Main (2).java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class faktor {
+  public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+ 
+ 
+  int n=scan.nextInt();
+   int m=scan.nextInt();
+   
+   
+   System.out.println((n*(m-1))+1);
+ 
+  }
+}
